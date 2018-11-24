37740cf6bd0dcb2e209ff11f88f9b5842b51be1a
kchsw <1031415601@qq.com>
2018-11-25 00:01:09 +0800

    11.24

--- music/src/components/singer-detail/singer-detail.vue
+++ music/src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,10 @@
 <template>
 	<transition name="slide">
-		
+		<music-list
+		  :title="title"
+		  :bg-image="bgImage"
+		  :songs="songs"
+		></music-list>
 	</transition>
 </template>
 
@@ -9,6 +13,7 @@
     import { getSingerDetail } from 'api/singer'
     import { ERR_OK } from 'api/config'
     import { createSong } from 'common/js/song'
+    import MusicList from 'components/music-list/music-list'
 
 	export default{
 		name: 'Singer-Detail',
@@ -18,33 +23,40 @@
 			}
 		},
 		components:{
-
+			MusicList
 	    },
 	    computed:{
 	    	...mapGetters([
 	    		'singer'
-	    	])
+	    	]),
+	    	title(){
+	    		return this.singer.name
+	    	},
+	    	bgImage(){
+	    		return this.singer.avatar
+	    	}
 	    },
 	    created(){
 	    	this._getDetail()
 	    },
 	    methods:{
 	    	_getDetail(){
+	    		if(!this.singer.id){
+	    			this.$router.push('/singer')
+	    			return
+	    		}
 	    		getSingerDetail(this.singer.id).then((res)=>{
 	    			if(res.code === ERR_OK){
-	    				console.log(res)
 	    				this.songs = this._normalizeSongs(res.data.list)
 	    			}
 	    		})
 	    	},
 	    	_normalizeSongs(list){
 	    		let ret = []
-	    		console.log(list)
 	    		list.forEach((item)=>{
 	    			let { musicData } = item
 	    			ret.push(createSong(musicData))
 	    		})
-	    		console.log(ret)
 	    		return ret
 
 	    	}

f131303a143a95e2d59e944dccb43d461ca51db1
kchsw <1031415601@qq.com>
2018-11-26 17:41:08 +0800

    11.26

--- music/src/App.vue
+++ music/src/App.vue
@@ -4,17 +4,20 @@
 		<tab></tab>
 		<keep-alive>
 			<router-view/>
-		</keep-alive>    
+		</keep-alive>
+		<player/>   
 	</div>
 </template>
 
 <script>
     import MHeader from './components/m-header/m-header'
     import Tab from './components/tab/tab'
+    import Player from 'components/player/player'
 	export default{
 		components:{
 			MHeader,
-			Tab
+			Tab,
+			Player
 		}
 	}
 </script>

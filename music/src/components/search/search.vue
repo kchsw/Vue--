36347a0d25f41581a0aee753545f443ab705d01f
kchsw <1031415601@qq.com>
2018-12-01 00:20:57 +0800

    12.1

--- music/src/components/search/search.vue
+++ music/src/components/search/search.vue
@@ -1,12 +1,91 @@
 <template>
-	<div>
-		search
+	<div class="search">
+		<div class="search-box-wrapper">
+			<search-box
+			  ref="searchBox"
+			  @query="onQuertChange"
+			></search-box>
+		</div>
+		<div class="shortcut-wrapper"
+		  ref="shortcutWrapper"
+		  v-show="!query"
+		>
+		    <div class="shortcut"
+		      ref="shortcut"
+		    >
+		        <div>
+		        	<div class="hot-key">
+		        		<h1 class="title">热门搜索</h1>
+		        		<ul>
+		        			<li class="item"
+		        			  v-for="item in hotKey"
+		        			  @click="addQuery(item.k)"
+		        			>
+		        				<span>{{item.k}}</span>
+		        			</li>
+		        		</ul>
+		        	</div>
+		        	<div class="search-history">
+		        		<h1 class="title">
+		        			<span class="text">搜索历史</span>
+		        			<span class="clear">
+		        				<i class="icon-clear"></i>
+		        			</span>
+		        		</h1>
+		        	</div>
+		        </div>	
+		    </div>
+		</div>
+		<div class="search-result"
+		  ref="searchResult"
+		  v-show="query"
+		>
+			
+		</div>
 	</div>
 </template>
 
 <script>
+    import SearchBox from 'base/search-box/search-box'
+    import Scroll from 'base/scroll/scroll'
+    import { getHotKey } from 'api/search'
+    import { ERR_OK } from 'api/config'
+    import { playlistMixin } from 'common/js/mixin'
+    import { mapActions } from 'vuex'
+
 	export default{
-		name: 'Search'
+		name: 'Search',
+		data(){
+			return{
+				hotKey: [],
+				query: '',
+				refreshDelay: 120
+			}
+		},
+		components:{
+			SearchBox,
+			Scroll
+		},
+		methods:{
+			_getHotKey(){
+				getHotKey().then((res)=>{
+					if(res.code === ERR_OK){
+						this.hotKey = res.data.hotkey.slice(0, 10)
+					}
+				})
+			},
+			addQuery(item){
+				console.log('11')
+				this.$refs.searchBox.setQuery(item)
+			},
+			
+			onQuertChange(query){
+				this.query = query.trim()
+			}
+		},
+		created(){
+			this._getHotKey()
+		}
 	}
 </script>
 
@@ -14,4 +93,50 @@
 @import '../../common/stylus/variable';
 @import '../../common/stylus/mixin';
 
+.search
+    .search-box-wrapper
+      margin: 20px
+    .shortcut-wrapper
+      position: fixed
+      top: 178px
+      bottom: 0
+      width: 100%
+      .shortcut
+        height: 100%
+        overflow: hidden
+        .hot-key
+          margin: 0 20px 20px 20px
+          .title
+            margin-bottom: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+          .item
+            display: inline-block
+            padding: 5px 10px
+            margin: 0 20px 10px 0
+            border-radius: 6px
+            background: $color-highlight-background
+            font-size: $font-size-medium
+            color: $color-text-d
+        .search-history
+          position: relative
+          margin: 0 20px
+          .title
+            display: flex
+            align-items: center
+            height: 40px
+            font-size: $font-size-medium
+            color: $color-text-l
+            .text
+              flex: 1
+            .clear
+              extend-click()
+              .icon-clear
+                font-size: $font-size-medium
+                color: $color-text-d
+    .search-result
+      position: fixed
+      width: 100%
+      top: 178px
+      bottom: 0
 </style>
